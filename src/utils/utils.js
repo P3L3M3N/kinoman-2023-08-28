e7e1a3b2e63760ca1d0c42921b594c06d37cce3a
Aleksandr Lobov <kz7055917158@gmail.com>
2023-08-30 18:07:00 +0300

    Adds a JSDoc for render function

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,12 @@
-export const render = (container, template, count = 1, place = `beforeend`) => {
+/**
+ * Отрисовывает HTML-шаблон в заданный DOM-контейнер указанное количество раз.
+ *
+ * @param {HTMLElement} container - DOM-элемент, в который будет отрисован шаблон.
+ * @param {string} template - HTML-шаблон в виде строки для отрисовки.
+ * @param {number} [count=1] - Количество раз, которое шаблон должен быть отрисован.
+ * @param {InsertPosition} [place='beforeend'] - Позиция внутри контейнера, где будет отрисован шаблон. Может принимать одно из следующих значений: 'beforebegin', 'afterbegin', 'beforeend', 'afterend'.
+ */
+export const render = (container, template, count = 1, place = 'beforeend') => {
   for (let i = 0; i < count; i++) {
     container.insertAdjacentHTML(place, template);
   }
