6f7ae3dfe73ae8db0acc1c8bbdb2f204d83b5389
Aleksandr Lobov <kz7055917158@gmail.com>
2023-08-30 16:36:58 +0300

    Adds JS module that renders upcoming movies section

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { render } from './utils/utils.js';
 import { createUserRankTmplate } from './layout/user-rank.js';
 import { createMenuTmplate } from './layout/menu.js';
 import { createSortListTmplate } from './layout/sort-list.js';
+import { createUpcomingMoviesTmplate } from './layout/upcoming-movies.js'
 
 const siteHeaderElement = document.querySelector(`.header`);
 const siteMainElement = document.querySelector(`.main`);
@@ -9,3 +10,4 @@ const siteMainElement = document.querySelector(`.main`);
 render(siteHeaderElement, createUserRankTmplate());
 render(siteMainElement, createMenuTmplate());
 render(siteMainElement, createSortListTmplate());
+render(siteMainElement, createUpcomingMoviesTmplate());
