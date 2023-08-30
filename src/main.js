ce0ce94369759704b12010e67502e9e7a4c4c332
Aleksandr Lobov <kz7055917158@gmail.com>
2023-08-30 18:18:51 +0300

    Rendering movie card (5 times) and show more button

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { render } from './utils/utils.js';
 import { createUserRankTmplate } from './layout/user-rank.js';
 import { createMenuTmplate } from './layout/menu.js';
 import { createSortListTmplate } from './layout/sort-list.js';
-import { createUpcomingMoviesTmplate } from './layout/upcoming-movies.js'
+import { createUpcomingMoviesTmplate, createMovieCardTemplate, createВuttonShowMoreTemplate } from './layout/upcoming-movies.js'
+
+const UPCOMING_MOVIES_CARD_COUNT = 5;
 
 const siteHeaderElement = document.querySelector(`.header`);
 const siteMainElement = document.querySelector(`.main`);
@@ -11,3 +13,10 @@ render(siteHeaderElement, createUserRankTmplate());
 render(siteMainElement, createMenuTmplate());
 render(siteMainElement, createSortListTmplate());
 render(siteMainElement, createUpcomingMoviesTmplate());
+
+const moviesBlockElement = siteMainElement.querySelector(`.films`);
+const moviesListElement = moviesBlockElement.querySelector(`.films-list`);
+const moviesContainerElement = moviesListElement.querySelector(`.films-list__container`);
+
+render(moviesContainerElement, createMovieCardTemplate (), UPCOMING_MOVIES_CARD_COUNT);
+render(moviesListElement, createВuttonShowMoreTemplate ());
