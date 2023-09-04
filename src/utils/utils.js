774f13cc5d169779a2cc9e654059c5a550452428
Aleksandr Lobov <kz7055917158@gmail.com>
2023-09-04 21:59:45 +0300

    Adds a JSDoc for render function (After mentor code review)

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,11 @@
+/**
+ * Отрисовывает HTML-шаблон в заданный контейнер.
+ *
+ * @param {Element} container DOM-элемент, в который будет отрисован шаблон.
+ * @param {string|string[]} template HTML-шаблон для вставки. Может быть массивом шаблонов.
+ * @param {string} [place=`beforeend`] Позиция вставки (соответствует параметрам метода `Element.insertAdjacentHTML`).
+ *
+ */
 export const render = (container, template, place = `beforeend`) => {
   if (Array.isArray(template)) {
     template.forEach((element) => {
