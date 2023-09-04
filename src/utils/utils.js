9f4a0e3ea6963d1a80bafff4d4127e2c12ec074c
Aleksandr Lobov <kz7055917158@gmail.com>
2023-09-04 21:16:39 +0300

    Fixing rendering function after mentor code review

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,13 +1,9 @@
-/**
- * Отрисовывает HTML-шаблон в заданный DOM-контейнер указанное количество раз.
- *
- * @param {HTMLElement} container - DOM-элемент, в который будет отрисован шаблон.
- * @param {string} template - HTML-шаблон в виде строки для отрисовки.
- * @param {number} [count=1] - Количество раз, которое шаблон должен быть отрисован.
- * @param {InsertPosition} [place='beforeend'] - Позиция внутри контейнера, где будет отрисован шаблон. Может принимать одно из следующих значений: 'beforebegin', 'afterbegin', 'beforeend', 'afterend'.
- */
-export const render = (container, template, count = 1, place = `beforeend`) => {
-  for (let i = 0; i < count; i++) {
-    container.insertAdjacentHTML(place, template);
+export const render = (container, template, place = `beforeend`) => {
+  if (Array.isArray(template)) {
+    template.forEach((element) => {
+      container.insertAdjacentHTML(place, element);
+    });
+    return;
   }
+  container.insertAdjacentHTML(place, template);
 };
