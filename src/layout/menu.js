dabcc17294199534149f98174681f7dc121f7aff
Aleksandr Lobov <kz7055917158@gmail.com>
2023-09-05 14:13:28 +0300

    Add changes after mentor code review, add JSDoc comments

--- src/layout/menu.js
+++ src/layout/menu.js
@@ -1,18 +1,25 @@
-const createMenuItemTemplate = (name, count, isActive) => {
+/**
+ * Создаёт HTML-шаблон для пункта меню.
+ *
+ * @param {string} name - Название пункта меню.
+ * @param {?number} [count=null] - Количество элементов, связанных с пунктом меню (null, если пкнкт не имеет элементов).
+ * @param {boolean} [isActive=false] - Указывает, активен ли пункт меню. По умолчанию "false".
+ * @return {string} HTML-шаблон пункта меню.
+ */
+const createMenuItemTemplate = (name, count = null, isActive = false) => {
   return (/* html */
-    `<a href="#${name.toLowerCase()}" class="main-navigation__item ${isActive ? `main-navigation__item--active` : ``}">
+    `<a href="#" class="main-navigation__item ${isActive ? `main-navigation__item--active` : ``}">
       ${name} ${count !== null ? `<span class="main-navigation__item-count">${count}</span>` : ``}
     </a>`
   );
 };
 
 export const createMenuTemplate = () => {
-
   const menuItems = [
     {name: `All movies`, count: null, isActive: true},
-    {name: `Watchlist`, count: 13, isActive: false},
-    {name: `History`, count: 4, isActive: false},
-    {name: `Favorites`, count: 8, isActive: false}
+    {name: `Watchlist`, count: 13},
+    {name: `History`, count: 4},
+    {name: `Favorites`, count: 8}
   ];
 
   const menuItemsTemplate = menuItems
@@ -28,3 +35,4 @@ export const createMenuTemplate = () => {
     </nav>`
   );
 };
+
